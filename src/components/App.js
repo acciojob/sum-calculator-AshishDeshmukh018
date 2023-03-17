8046bf439dc19115d7e918abe5f16a6ef99f718f
AshishDeshmukh018 <126492153+AshishDeshmukh018@users.noreply.github.com>
2023-03-17 18:25:42 +0530

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,40 @@
-
+import React, { useState, useEffect } from 'react';
 import React from "react";
 import './../styles/App.css';
 
-const App = () => {
+function NumberInput() {
+  const [numbers, setNumbers] = useState([]);
+  const [sum, setSum] = useState(0);
+
+  useEffect(() => {
+    const calculateSum = async () => {
+      const newSum = await sumNumbers(numbers);
+      setSum(newSum);
+    };
+    calculateSum();
+  }, [numbers]);
+
+  const sumNumbers = async (numbers) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const newSum = numbers.reduce((acc, cur) => acc + Number(cur), 0);
+        resolve(newSum);
+      }, 1000);
+    });
+  };
+
+  const handleInputChange = (event) => {
+    const { value } = event.target;
+    setNumbers((prevNumbers) => [...prevNumbers, value]);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <label htmlFor="number-input">Enter numbers:</label>
+      <input type="number" id="number-input" onChange={handleInputChange} />
+      <p>Sum: {sum}</p>
     </div>
-  )
+  );
 }
 
 export default App
